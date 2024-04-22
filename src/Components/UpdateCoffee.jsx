aab271a03169c44621aaa039a94208077a379594
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-04-22 14:31:59 +0600

    added plugins and create two components.

---
+++ src/Components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h3>Update a coffee</h3>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './Components/AddCoffee.jsx';
+import UpdateCoffee from './Components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee/>,
+  },
+  {
+    path: "/updateCoffee",
+    element: <UpdateCoffee/>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
